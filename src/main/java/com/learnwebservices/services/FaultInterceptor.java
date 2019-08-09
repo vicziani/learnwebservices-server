c8af543ff16151b33a77a71d409459677144db71
vicziani <viczian.istvan@gmail.com>
2019-08-09 21:03:28 +0200

    Without System.out

--- src/main/java/com/learnwebservices/services/FaultInterceptor.java
+++ src/main/java/com/learnwebservices/services/FaultInterceptor.java
@@ -14,7 +14,6 @@ public class FaultInterceptor extends AbstractSoapInterceptor {
     @Override
     public void handleMessage(SoapMessage message) throws Fault {
         Exception exception = message.getContent(Exception.class);
-        System.out.println(message.getContentFormats());
         if (exception instanceof Fault && isWrongNumber(exception)) {
             ((Fault) exception).setMessage(exception.getMessage() +
                     " If you use comma (,) character for decimal separator, change it to dot (.) character!");
