6fd1446c172e835b40eca8653735adac349849aa
Istvan Viczian <viczian.istvan@gmail.com>
2026-01-03 22:53:13 +0100

    Collect metadata with native-image-agent

--- src/main/java/com/learnwebservices/CxfNativeConfig.java
+++
@@ -1,9 +0,0 @@
-package com.learnwebservices;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportRuntimeHints;
-
-@ImportRuntimeHints(CxfResourceHints.class)
-@Configuration
-public class CxfNativeConfig {
-}
